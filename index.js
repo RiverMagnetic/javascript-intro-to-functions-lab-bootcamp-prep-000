d691bcec26da4d409c9e5e8dc52ddd135f80aca5
River Magnetic <magneticcontinuum@gmail.com>
2018-01-09 17:44:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string)
-    console.log(string.toLowerCase())
-  }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var string = 'hello!'
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can't hear you!";
+}else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+}else if (string === "I love you, Grandma.") {
+  return "I love you, too.";
+}
+}
